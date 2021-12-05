56a84acb3746f01e5fa90a51f4c00c62215834bc
Siddharth <93863822+sidstargem@users.noreply.github.com>
2021-12-05 19:23:52 +0000

    Add files via upload

---
+++ curcumference calculator.py
@@ -0,0 +1,3 @@
+radius = float(input("Enter the radius of the circle I'll give you the CIRCUMFERENCE: "))
+circumference = 2*22/7*radius
+print("The circumference of the circle is:",circumference,"")
